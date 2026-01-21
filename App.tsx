24c3d43383e3ab970b390c55f9cc6206ba76ad82
trongkhiemk3 <153691556+trongkhiemk3@users.noreply.github.com>
2026-01-21 17:52:00 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -293,7 +293,7 @@ const App: React.FC = () => {
       
       <footer className="mt-8 text-center text-slate-400 text-sm max-w-2xl">
         <p>
-          Game được tạo bởi Trọng Khiêm. Mọi kết quả được lưu trữ tại: {window.location.hostname}
+          Game được tạo bởi Trọng Khiêm. Mọi kết quả được lưu trữ tại Firebase và tôi có quyền chỉnh sửa.
         </p>
       </footer>
     </div>
